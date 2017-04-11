d17114ddd0ff1f24463d000f6f23d067716051d6
Kelvin <Kelvin@Disuto-Workstation>
2017-04-11 19:17:31 -0400

    Moved everything from the extended class to its superclass.

--- src/main/java/com/bkj/banking/CheckingAccount.java
+++ src/main/java/com/bkj/banking/CheckingAccount.java
@@ -2,49 +2,110 @@ package com.bkj.banking;
 
 /**
  * Created by bclaus on 4/6/17.
+ * Modified by Kelvin Gomez on 4/10/2017.
  */
 public class CheckingAccount extends Account {
-
+ /**
+     * Properties. -KG
+     */
+    private final double withdrawFee = 3.0d;
+    private final double overdraftFee = 35.0d;
+    private double initialBalance;
+    private String customerId;
+    private String accountDescription;
     /**
-     * Create a new checking account
+     * Constructor
      * @param initialBalance
      * @param customerId
-     * @param accountDescription
+     * @param accountDescription 
      */
-    CheckingAccount (double initialBalance, String customerId, String accountDescription){
+    public CheckingAccount(double initialBalance, String customerId, String accountDescription) {
 
     }
-
+    /*****************************
+     *BEGIN: Getter & Setters. -KG
+     */
     /**
-     * Adds a transaction "FEE" based on this account's monthly and other fees.
+     * Getter
+     * @return 
+     */
+    public double getInitialBalance() {
+        return initialBalance;
+    }
+    /**
+     * Setter
+     * @param initialBalance 
+     */
+    public void setInitialBalance(double initialBalance) {
+        this.initialBalance = initialBalance;
+    }
+    /**
+     * Getter
+     * @return 
+     */
+    public String getCustomerId() {
+        return customerId;
+    }
+    /**
+     * Setter
+     * @param customerId 
+     */
+    public void setCustomerId(String customerId) {
+        this.customerId = customerId;
+    }
+    /**
+     * Getter
+     * @return 
+     */
+    public String getAccountDescription() {
+        return accountDescription;
+    }
+    /**
+     * Setter
+     * @param accountDescription 
+     */
+    public void setAccountDescription(String accountDescription) {
+        this.accountDescription = accountDescription;
+    }
+    /*****************************
+     *END: Getter & Setters. -KG
+     */
+    
+    /**
+     * Adds a transaction "FEE" based on this accounts monthly and other fees.
      * @param amount
      */
     void applyFee( double amount ){
-
+        initialBalance = initialBalance - amount;
+        if(initialBalance < 0.0d){
+            initialBalance = initialBalance - overdraftFee;
+        }
     }
-
     /**
      * Add money into account
-     *
-     * @param amount
+     * @param amount 
      */
     @Override
-    public void deposit(double amount) {
-
+    public void deposit(double amount){
+        initialBalance = initialBalance + amount;
     }
-
     /**
      * remove money from account
-     *
      * @param amount
      */
     @Override
     public void withdraw(double amount) {
-
+        initialBalance = initialBalance - amount;
+        applyFee(withdrawFee);
     }
-
-    /*
-    Getters, setters, (for example, to get and set the overdraft fee), toString, and other methods as needed (for example, inherited abstract methods)
-    You need to test any non-trivial methods your group decides are a good idea.
-    */
+    /**
+     * toString Override
+     * @return 
+     */
+    @Override
+    public String toString() {
+        return "Checkings{" + "Initial Balance = $" + initialBalance +
+                ", Customer Id=" + customerId + ", Account Description=" +
+                accountDescription + '}';
+    }
 }
