98ec2dfe51ae9ff44fa444b2c9012ae18151c1c4
Kelvin <Kelvin@Disuto-Workstation>
2017-04-12 12:45:34 -0400

    Added an additional test for overdraft fees.

--- src/test/java/com/bkj/banking/SavingsAccountTest.java
+++ src/test/java/com/bkj/banking/SavingsAccountTest.java
@@ -4,6 +4,7 @@ import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
+import static org.junit.Assert.assertFalse;
 import org.junit.runner.JUnitCore;
 /**
  *
@@ -38,7 +39,8 @@ public class SavingsAccountTest {
      */
     @Test
     public void addInterestTest(){
-        SavingsAccount sav = new SavingsAccount(3389.37, "GOM7876846", "Savings Account");
+        SavingsAccount sav = new SavingsAccount(3389.37, 
+                "GOM7876846", "Savings Account");
         sav.setMonthlyInterestRate(3); //Should set rate of 3% to 0.03
         sav.getInitialBalance(); //Should return 3389.37
         sav.addInterest(5); //Adds interest based on 5 year for time.
@@ -58,7 +60,8 @@ public class SavingsAccountTest {
      */
     @Test
     public void depositTest(){
-        SavingsAccount sav = new SavingsAccount(3389.37, "GOM7876846", "Savings Account"); 
+        SavingsAccount sav = new SavingsAccount(3389.37, 
+                "GOM7876846", "Savings Account"); 
         sav.getInitialBalance(); //Should return 3389.37
         sav.deposit(1000.63); // Should increase balance to 4389.37
         
@@ -71,13 +74,26 @@ public class SavingsAccountTest {
      */    
     @Test
     public void withdrawTest(){
-        SavingsAccount sav = new SavingsAccount(3389.37, "GOM7876846", "Savings Account"); 
+        SavingsAccount sav = new SavingsAccount(3389.37, 
+                "GOM7876846", "Savings Account"); 
         sav.getInitialBalance(); //Should return 3389.37
         sav.withdraw(389.37); // Should decrease balance to 3000.00
         
         assertFalse("Balance decreased to 3000.00",
                 sav.getCurrentBalance() == 3000.00);
     }
+    /**
+     * Testing application of overdraft fee. -KG
+     */    
+    @Test
+    public void withdrawWithOverdraftFeeTest(){
+        SavingsAccount sav = new SavingsAccount(100.0d, 
+                "GOM20047", "Checkings Account");
+        sav.withdraw(125.0d);
+        
+        assertFalse("Ending balance is -$60.00", 
+                sav.getInitialBalance() == -60.0d);
+    }    
 
     public static void main ( String [] args ){
         JUnitCore.main( "MyAppTestSuite" ); 
